a48939c69130b5915a3a1a1af644c41268a538d5
james hawken <james_1999@live.fr>
2019-12-08 17:14:28 +0100

    collection view

---
+++ MovieDatabase/UI/ActorCollectionViewCell.swift
@@ -0,0 +1,9 @@
+//
+//  ActorCollectionViewCell.swift
+//  MovieDatabase
+//
+//  Created by James Hawken on 08/12/2019.
+//  Copyright © 2019 Gil Nakache. All rights reserved.
+//
+
+import Foundation
